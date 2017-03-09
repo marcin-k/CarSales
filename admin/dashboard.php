2fbff646b7a4bebbc0215ce6581ccce1f2f5787b
Mark Saunders <mark@misterhoots.com>
2017-03-09 21:09:18 +0000

    added dashboard.php

---
+++ admin/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+    include 'inc/header.php';
+    include "admin_db_operations.php";
+?>
+<!--GET HEADER-->
+
+
+<main>
+  <section>
+    <h3>Dashboard</h3>
+    <h4>Welcome Mark</h4>
+    <form action="">
+      <button class="small"></button>
+      <button class="small"></button>
+      <button class="small"></button>
+    </form>
+  </section>
+</main>
+
+
+<!--GET FOOTER-->
+<?php include 'inc/footer.php'; ?>
